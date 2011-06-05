03981c6a9a2fde6f6ba5c0aeb4d32606eb6c042e
Pawel Pabich <pawel@pabich.eu>
2011-06-05 16:00:29 +1000

    Some e2e functionality is working now

--- CodeMetricsReportProcessor/Program.cs
+++ CodeMetricsReportProcessor/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using RazorEngine.Templating;
 
 namespace CodeMetricsReportProcessor
 {
@@ -9,6 +10,42 @@ namespace CodeMetricsReportProcessor
     {
         static void Main(string[] args)
         {
+            var system = Console.ForegroundColor;
+
+            try
+            {
+                var reportGenerator = new ReportGenerator();
+                reportGenerator.GenerateFullReport(@"C:\Temp\MetricTest\out.xml", @"C:\Temp\MetricTest\Results");
+
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("All good");
+
+            }
+            catch(TemplateCompilationException ex)
+            {
+                var message = "Errors: ";
+                foreach (var compilerError in ex.Errors)
+                {
+                    message += Environment.NewLine + compilerError.ToString();
+                }
+
+                LogException(ex, message);
+            }
+            catch (Exception e)
+            {
+                LogException(e, "Something went bad.");
+            }
+            finally
+            {
+                Console.ForegroundColor = system;
+            }
+
+        }
+
+        private static void LogException(Exception e, string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message + Environment.NewLine + e.ToString());
         }
     }
 }
--- CodeMetricsReportProcessor/ReportGenerator.cs
+++ CodeMetricsReportProcessor/ReportGenerator.cs
@@ -12,11 +12,37 @@ namespace CodeMetricsReportProcessor
     {
         public void GenerateFullReport(string codeMetricsDataFile, string reportOutputFolder)
         {
-            var data = new CodeMetricsParser().Parse(GetContent(codeMetricsDataFile));
+            CopyTemplates(reportOutputFolder);
+
+            var parser = new CodeMetricsParser();
+            var data = parser.Parse(GetContent(codeMetricsDataFile));
 
             var renderer = new TemplateRenderer();
-            var summary = renderer.Render(GetContent(@"Templates\Summary.html"), data);
+            var summary = renderer.Render(GetContent(reportOutputFolder, "Summary.template.html"), data);
             SaveContent(reportOutputFolder, "Summary.html", summary);
+        }
+
+        private void CopyTemplates(string to)
+        {
+            var from = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Templates");
+            if (Directory.Exists(to))
+            {
+                Directory.Delete(to, true);               
+            }
+
+            Directory.CreateDirectory(to);
+
+            foreach (var fromFolderPath in Directory.GetDirectories(from, "*", SearchOption.AllDirectories))
+            {
+                var toFolderPath = fromFolderPath.Replace(from, to);
+                Directory.CreateDirectory(toFolderPath);
+            }
+
+            foreach (var fromFilePath in Directory.GetFiles(from, "*.*", SearchOption.AllDirectories))
+            {
+                var toFilePath = fromFilePath.Replace(from, to);
+                File.Copy(fromFilePath, toFilePath, true);
+            }
         }
 
         private void SaveContent(string pathToFolder, string fileName, string content)
@@ -28,6 +54,11 @@ namespace CodeMetricsReportProcessor
         private string GetContent(string pathToFile)
         {
             return File.ReadAllText(pathToFile);
+        }
+
+        private string GetContent(string pathToFolder, string fileName)
+        {
+            return GetContent(Path.Combine(pathToFolder, fileName));
         }
     }
 }
